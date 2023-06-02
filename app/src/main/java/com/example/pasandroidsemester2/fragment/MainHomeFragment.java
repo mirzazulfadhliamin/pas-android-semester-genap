0d83a8073fd4d3e46061088d81a60b1163d29f5c
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2023-06-02 22:07:12 +0700

    Pake ViewPager2 sama fix bug crash resume fragment home

--- app/src/main/java/com/example/pasandroidsemester2/fragment/MainHomeFragment.java
+++ app/src/main/java/com/example/pasandroidsemester2/fragment/MainHomeFragment.java
@@ -5,48 +5,94 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.pasandroidsemester2.R;
+import com.example.pasandroidsemester2.databinding.FragmentMainHomeBinding;
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainHomeFragment extends Fragment {
 
+    private FragmentMainHomeBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_home, container, false);
-    }
+        binding = FragmentMainHomeBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        MyAdapter adapter = new MyAdapter(getChildFragmentManager());
-        ViewPager viewPager = view.findViewById(R.id.viewPager);
+        HomeTabAdapter adapter = new HomeTabAdapter(this);
+        ViewPager2 viewPager = binding.viewPager;
+        viewPager.setSaveEnabled(false);
         viewPager.setAdapter(adapter);
 
-        TabLayout tabLayout = view.findViewById(R.id.TabLayout);
-        tabLayout.setupWithViewPager(viewPager);
+        TabLayout tabLayout = binding.TabLayout;
+        new TabLayoutMediator(tabLayout,
+                viewPager,
+                (tab, position) -> {
+                    switch (position) {
+                        case 0: {
+                            tab.setText("HOME");
+                            break;
+                        }
+                        case 1: {
+                            tab.setText("ANIME");
+                            break;
+                        }
+                        default: break;
+                    }
+                }).attach();
+
+        return view;
     }
 
-    class MyAdapter extends FragmentStatePagerAdapter {
+    class HomeTabAdapter extends FragmentStateAdapter {
 
-        public MyAdapter(FragmentManager fm) {
-            super(fm);
+        public HomeTabAdapter(Fragment fragment) {
+            super(fragment);
         }
 
-        // Return Fragment to display for that page
+//        @NonNull
+//        @Override
+//        public Fragment getItem(int position) {
+//            switch (position) {
+//                case 0:
+//                    return new HomePopularFragment();
+//                case 1:
+//                    return new HomeAnimeFragment();
+//                default:
+//                    return null;
+//            }
+//        }
+
+        // Udh gk perlu sama sekali
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//            switch (position) {
+//                case 0:
+//                    return "HOME";
+//                case 1:
+//                    return "Anime";
+//
+//                default:
+//                    return null;
+//            }
+//        }
+
+//        @Override
+//        public int getCount() {
+//            return 2;
+//        }
+
+        // Pengganti getItem()
         @NonNull
         @Override
-        public Fragment getItem(int position) {
+        public Fragment createFragment(int position) {
             switch (position) {
                 case 0:
                     return new HomePopularFragment();
@@ -57,22 +103,9 @@ public class MainHomeFragment extends Fragment {
             }
         }
 
-        // Will be displayed as the tab's label
+        // Pengganti getCount()
         @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "HOME";
-                case 1:
-                    return "Anime";
-
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
+        public int getItemCount() {
             return 2;
         }
     }
